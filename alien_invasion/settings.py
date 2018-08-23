6f591d4b27bd72b5b9b06e9d5f32fedb89180744
yang85yang <2300472298@qq.com>
2018-08-23 14:52:12 +0800

    Create settings.py

---
+++ alien_invasion/settings.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+class Settings():
+    """ 存储　所有的设置的类"""
+    def __init__(self):
+        """初始化游戏的设置"""
+        self.screen_width = 480
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
