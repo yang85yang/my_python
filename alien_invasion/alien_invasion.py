b7c4f0a4891bbe70af7f3a15ddd882ef0239a3d0
Your Name <you@example.com>
2018-08-23 22:10:09 +0800

    add ship in screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
-import sys
+
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     #初始化
@@ -11,6 +13,9 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #创建飞船
+    ship = Ship(screen)
+
     #设置背景
     bg_color = (230, 230, 230)
 
@@ -18,13 +23,8 @@ def run_game():
     #开始游戏
     while True:
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
         #每次循环都重新绘制屏幕
-        screen.fill(bg_color)
-
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
---
+++ alien_invasion/game_functions.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+import sys
+import  pygame
+
+def check_events():
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    """更新屏幕"""
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
