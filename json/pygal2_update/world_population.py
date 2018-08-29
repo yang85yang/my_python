6839f5861b0a71b1011f1ec7d84fc1ae72b39f37
yanghj <yanghj@gqy.com.cn>
2018-08-29 17:07:45 +0800

    chapter 17

--- json/pygal2_update/world_population.py
+++ json/pygal2_update/world_population.py
@@ -1,6 +1,6 @@
 import json
 
-import pygal
+from pygal.maps.world import World
 from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
 from country_codes import get_country_code
@@ -34,7 +34,8 @@ for cc, pop in cc_populations.items():
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
 wm_style = RS('#336699', base_style=LCS)
-wm = pygal.Worldmap(style=wm_style)
+wm = World(style=wm_style)
+wm.force_uri_protocol = 'http'
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn', cc_pops_2)
