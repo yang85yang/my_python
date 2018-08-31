2ef39b4204ebd46c73be20b99dbc0ceb63f78850
yanghj <yanghj@gqy.com.cn>
2018-08-31 17:05:50 +0800

    page 191

---
+++ django/learning_log/learning_logs/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from learning_logs.models import Topic
+admin.site.register(Topic)
+
+# Register your models here.
